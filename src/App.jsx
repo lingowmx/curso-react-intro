dd26fd206dabc6dcb841b82b6ee241037c946a07
lingowmx <clingow@hotmail.com>
2025-02-24 20:26:26 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Counter } from './components/Counter'
 import { Search } from './components/Search'
 import './App.css'
@@ -13,12 +14,16 @@ function App() {
     {text:"Third activit", isCompleted: false},
     {text:"Fourth activity", isCompleted: true},
   ]
+  const [searchValue, setSearchValue] = useState('')
+  console.log('Los usuarios buscaron todos de'  + searchValue)
 
   return (
     <>
     <div className='p-8 w-full h-screen flex flex-col text-center'>
       <Counter completed={8} totalTodos={10}/>
-      <Search />
+      <Search 
+        searchValue={searchValue} 
+        setSearchValue={setSearchValue}/>
       <List>
         {defaultTodos.map((todo) => (
           <TodoItem 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,21 @@
+import PropTypes from "prop-types"
 
-
-export const Search = () => {
+export const Search = ({searchValue, setSearchValue}) => {
+  
   return (
     <div className="border border-gray-400 rounded-lg">
-      <input type="text" placeholder="Search your todos" className="flex pl-4 rounded-lg" />
+      <input 
+      type="text"
+      placeholder="Search your todos"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value)
+      }}
+      className="flex pl-4 rounded-lg" />
     </div>
   )
+}
+Search.propTypes = {
+  searchValue: PropTypes.string.isRequired,
+  setSearchValue: PropTypes.string.isRequired
 }
