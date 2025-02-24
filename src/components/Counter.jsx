f36fe9cc7160e300acb24ea15dc0d1e6ad0acd06
lingowmx <clingow@hotmail.com>
2025-02-24 16:57:44 +0100

    proptypes install

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import PropTypes from 'prop-types';
 export const Counter = ({completed, totalTodos}) => {
   return (
     <div>
       <h1>Has completado {completed} de {totalTodos} TODOs</h1>
     </div>
   )
+}
+
+Counter.propTypes ={
+  completed: PropTypes.number.isRequired,
+  totalTodos: PropTypes.number.isRequired
 }
 
