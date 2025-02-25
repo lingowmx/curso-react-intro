485d94b3940a4f87e3f7c9a9f6a9f35569e9eddc
lingowmx <clingow@hotmail.com>
2025-02-25 09:09:09 +0100

    first derive states

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,24 @@ function App() {
   const defaultTodos = [
     {text:"First activity", isCompleted: false},
     {text:"Second actividy", isCompleted: false},
-    {text:"Third activit", isCompleted: false},
+    {text:"Third activit", isCompleted: true},
     {text:"Fourth activity", isCompleted: true},
+    {text:"Fifth activity", isCompleted:true}
   ]
   const [searchValue, setSearchValue] = useState('')
   console.log('Los usuarios buscaron todos de'  + searchValue)
 
+  const [todos, setTodos] = useState(defaultTodos)
+  
+  const completedTodos = todos.filter(todo => !!todo.isCompleted).length
+  const totalTodos = todos.length
+
   return (
     <>
     <div className='p-8 w-full h-screen flex flex-col text-center'>
-      <Counter completed={8} totalTodos={10}/>
+      <Counter 
+      completed={completedTodos}
+      totalTodos={totalTodos}/>
       <Search 
         searchValue={searchValue} 
         setSearchValue={setSearchValue}/>
