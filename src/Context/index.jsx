50903133012b0cc464b88d9a3921ab11c88e87f4
lingowmx <clingow@hotmail.com>
2025-02-27 17:15:44 +0100

    addTodo completed

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,44 +4,52 @@ import { createContext } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
 export const TodoContext = createContext()
-export const TodoProvider = ({children}) => {
-    const {
-      item: todos,
-      savedItem: savedTodos,
-      loading,
-      error,
-    } = useLocalStorage('Todos_V1', [])
-    const [searchValue, setSearchValue] = useState('')
-    const [openModal, setOpenModal] = useState(false)
-  
-    const completedTodos = todos.filter(todo => !!todo.isCompleted).length
-    const totalTodos = todos.length
-    const searchedTodos = todos.filter(
-      (todo) => {
-        const todoText = todo.text.toLowerCase();
-        const searchText = searchValue.toLocaleLowerCase();
-        return todoText.includes(searchText)
-      }
+export const TodoProvider = ({ children }) => {
+  const {
+    item: todos,
+    savedItem: savedTodos,
+    loading,
+    error,
+  } = useLocalStorage('Todos_V1', [])
+  const [searchValue, setSearchValue] = useState('')
+  const [openModal, setOpenModal] = useState(false)
+
+  const completedTodos = todos.filter(todo => !!todo.isCompleted).length
+  const totalTodos = todos.length
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLocaleLowerCase();
+      return todoText.includes(searchText)
+    }
+  )
+  const addTodo = (text) => {
+    const newTodos = [...todos]
+    newTodos.push({
+      text,
+      completed: false
+    })
+    savedTodos(newTodos)
+  }
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
     )
-  
-    const completeTodo = (text) => {
-      const newTodos = [...todos]
-      const todoIndex = newTodos.findIndex(
-        (todo) => todo.text === text
-      )
-      newTodos[todoIndex].isCompleted = !newTodos[todoIndex].isCompleted
-      savedTodos(newTodos)
-    }
-  
-    const deleteTodo = (text) => {
-      const newTodos = [...todos]
-      const todoIndex = newTodos.findIndex(
-        (todo) => todo.text === text
-      )
-      newTodos.splice(todoIndex, 1)
-      savedTodos(newTodos)
-    }
-  
+    newTodos[todoIndex].isCompleted = !newTodos[todoIndex].isCompleted
+    savedTodos(newTodos)
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    )
+    newTodos.splice(todoIndex, 1)
+    savedTodos(newTodos)
+  }
+
   return (
     <TodoContext.Provider value={{
       completedTodos,
@@ -54,7 +62,8 @@ export const TodoProvider = ({children}) => {
       loading,
       error,
       openModal,
-      setOpenModal
+      setOpenModal,
+      addTodo,
     }}>
       {children}
     </TodoContext.Provider>
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,18 +1,24 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { TodoContext } from "../Context"
 
 export const TodoForm = () => {
   const {
+    addTodo,
     setOpenModal
   } = useContext(TodoContext)
+  const [newTodoValue, setNewTodoValue] = useState('')
 
   const onSubmit = (e) => {
     e.preventDefault(),
+    addTodo(newTodoValue)
     setOpenModal(false)
   }
   const onCancel = (e) => {
     e.preventDefault()
     setOpenModal(false)
+  }
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value)
   }
   return (
     <form 
@@ -20,8 +26,11 @@ export const TodoForm = () => {
       className="border border-black flex flex-col items-center w-80 rounded-lg bg-cyan-200">
       <label className="mt-1">Crea tu primer TODO</label>
       <textarea 
+        value={newTodoValue}
+        onChange={onChange}
         className="m-2 rounded-lg resize-none pl-2"
-        placeholder="textAREA"/>
+        placeholder="textAREA"
+        required/>
         <div className="flex justify-around  w-full mt-4 mb-2">
           <button
             type="button"
