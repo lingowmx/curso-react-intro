01c0c60158cda326dda68f869aca50211c7e6837
lingowmx <clingow@hotmail.com>
2025-02-28 13:12:27 +0100

    added confetti justo to check how it works

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,27 @@
 import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { TodoContext } from '../Context';
+import Confetti from "react-confetti"
 
 export const Counter = () => {
   const {
-      completedTodos,
-      totalTodos,
+    completedTodos,
+    totalTodos,
   } = useContext(TodoContext)
+  const allCompleted = totalTodos === completedTodos
   return (
     <div>
-      {totalTodos === completedTodos ? "Felicidades en grandote!!" :
-      <h1 className='text-2xl'>Has completado 
-         <span className='font-semibold'> {completedTodos}</span> de 
-         <span className='font-semibold'> {totalTodos}</span>  TODOs
-      </h1>
+      {allCompleted && <Confetti width={window.innerWidth} height={window.innerHeight} numberOfPieces={300} />}
+      {allCompleted
+        ? (
+          <h1 className='text-2xl shadow-md rounded-3xl shadow-green-500'>
+            All Todos Done 👍
+          </h1>
+        ) :
+        <h1 className='text-2xl'>Has completado
+          <span className='font-semibold'> {completedTodos}</span> de
+          <span className='font-semibold'> {totalTodos}</span>  TODOs
+        </h1>
       }
     </div>
   )
